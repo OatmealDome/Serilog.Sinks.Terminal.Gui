991aa77c9c860199e4f22f6eb704056267696409
OatmealDome <julian@oatmealdome.me>
2021-06-26 15:22:07 -0400

    Initial commit

---
+++ Serilog.Sinks.Terminal.Gui/TextViewSink.cs
@@ -0,0 +1,43 @@
+﻿using NStack;
+using Serilog.Core;
+using Serilog.Events;
+using System;
+using System.Reflection;
+using Terminal.Gui;
+
+namespace Serilog.Sinks.Terminal.Gui
+{
+    public class TextViewSink : ILogEventSink
+    {
+        private IFormatProvider Provider;
+        private TextView View;
+        private MethodInfo InsertTextMethod;
+
+        public TextViewSink(TextView view, IFormatProvider provider = null)
+        {
+            Provider = provider;
+            View = view;
+
+            // We should not allow the TextView to be focused. Scrolling can cause problems.
+            View.CanFocus = false;
+
+            // Fetch the InsertText(ustring text) method. For whatever reason, there is no API for appending to the
+            // TextView. "TextView.Text += newText" does work, but it doesn't scroll the TextView to the bottom
+            // like I want it to. So, we're stuck with this gigantic hack for now.
+            InsertTextMethod = view.GetType().GetMethod("InsertText", BindingFlags.Instance | BindingFlags.NonPublic,
+                                                        null, new Type[] { typeof(ustring) }, null);
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+            string formattedMessage = logEvent.RenderMessage(Provider) + "\n";
+
+            Application.MainLoop?.Invoke(() =>
+            {
+                InsertTextMethod.Invoke(View, new object[] { ustring.Make(formattedMessage) });
+                View.SetNeedsDisplay();
+            });
+        }
+
+    }
+}
---
+++ Serilog.Sinks.Terminal.Gui/TextViewSinkExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Serilog.Configuration;
+using System;
+using Terminal.Gui;
+
+namespace Serilog.Sinks.Terminal.Gui
+{
+    public static class TextViewSinkExtensions
+    {
+        public static LoggerConfiguration TextViewSink(
+                  this LoggerSinkConfiguration loggerConfiguration,
+                  TextView textView,
+                  IFormatProvider formatProvider = null)
+        {
+            return loggerConfiguration.Sink(new TextViewSink(textView, formatProvider));
+        }
+
+    }
+}
